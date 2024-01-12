d4dacb5885e6d781b52728e0c4d1ab4ef38d44d4
thesimplezero <hi@thesimplezero.io>
2024-01-12 03:33:06 +0300

    fixed 2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Module with a function 'floor' that computes the floor of a float.
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Computes the floor of a float.
+
+    Args:
+        n (float): Input float number.
+
+    Returns:
+        int: Floor of the input float.
+    """
+    return math.floor(n)
