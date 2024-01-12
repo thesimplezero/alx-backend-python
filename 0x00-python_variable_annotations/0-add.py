6ada5370e5e816b75ff42e2d2191dcdf287b45ae
thesimplezero <hi@thesimplezero.io>
2024-01-12 03:33:06 +0300

    updated 0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module with 'add' function. It takes two floats and returns their sum.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Function to add two floats.
+
+    Args:
+        a (float): First number.
+        b (float): Second number.
+
+    Returns:
+        float: Sum of a and b.
+    """
+    return a + b
