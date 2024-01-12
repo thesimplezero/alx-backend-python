79e6adaa0e5741d659f282ba53bb007504514ca9
thesimplezero <hi@thesimplezero.io>
2024-01-12 03:33:06 +0300

    modified 3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module with 'to_str' function that converts a float to a string.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Converts a float to a string.
+
+    Args:
+        n (float): Input float.
+
+    Returns:
+        str: String representation of the float.
+    """
+    return str(n)
