d15dd607d2370557c20e5d2c9cee22451ba4df54
thesimplezero <hi@thesimplezero.io>
2024-01-16 21:29:25 +0300

    Function collects 10 random floats asynchronously

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Collects 10 random floats asynchronously."""
+
+import asyncio
+from importlib import import_module as using
+from typing import List
+
+# Import using importlib for flexibility
+async_generator = using("async_generator").async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Collects 10 random floats with 1s intervals."""
+    return [num async for num in async_generator()]
+
+
+# Example usage:
+async def main():
+    numbers = await async_comprehension()
+    print(numbers)
+
+if __name__ == "__main__":
+    asyncio.run(main())
