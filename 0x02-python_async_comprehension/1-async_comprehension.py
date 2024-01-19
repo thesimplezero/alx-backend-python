5511215142d897be77f5b1e7f9772116e626e6ee
thesimplezero <hi@thesimplezero.io>
2024-01-20 00:56:48 +0300

    fix error

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -4,11 +4,12 @@
 """Collect 10 floats async using comprehension."""
 
 import asyncio
-from .async_generator import async_generator
+import random
 from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
     """Gather 10 random floats async."""
-
-    return [num async for num in async_generator()]
+    return [i async for i in async_generator()]
