a06a769d5664bb8953fc12228ce74cca128d25e4
thesimplezero <hi@thesimplezero.com>
2024-02-05 19:55:03 +0300

    edit test utils script

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,48 +1,20 @@
 #!/usr/bin/env python3
-"""
-Module containing unit tests for utils.py
-"""
 
 import unittest
-from unittest.mock import patch
-from utils import memoize
+from parameterized import parameterized
+from utils import access_nested_map
 
+class TestAccessNestedMap(unittest.TestCase):
+    """Class that tests if the access_nested_map function works"""
 
-class TestMemoize(unittest.TestCase):
-    """
-    Test cases for memoize decorator
-    """
+    @parameterized.expand([
+        ({"a": {"b": {"c": 4}}}, ["a", "b", "c"], 4),
+        ({"a": {"b": {"c": 4}}}, ["a", "b", "d"], None),
+        ({"a": {"b": {"c": 4}}}, ["x", "y", "z"], None)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """Tests if the function returns the correct value"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
 
-    class TestClass:
-        """
-        Test class with a_method and a_property decorated with memoize
-        """
-
-        def a_method(self):
-            return 42
-
-        @memoize
-        def a_property(self):
-            return self.a_method()
-
-    def test_memoize(self):
-        """
-        Test memoize decorator
-        """
-        # Create instance of TestClass
-        test_instance = self.TestClass()
-
-        # Patch a_method to return expected value
-        with patch.object(test_instance, 'a_method') as mock_a_method:
-            mock_a_method.return_value = 42
-
-            # Call a_property twice
-            result1 = test_instance.a_property
-            result2 = test_instance.a_property
-
-            # Assert that a_method is called once
-            mock_a_method.assert_called_once()
-
-            # Assert that results are correct
-            self.assertEqual(result1, 42)
-            self.assertEqual(result2, 42)
+if __name__ == '__main__':
+    unittest.main()
